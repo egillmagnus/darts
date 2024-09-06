1c46c295bd8076ff419f19ac24502626b89ec986
BuildTools <unconfigured@null.spigotmc.org>
2024-09-06 13:19:47 +0000

    initial commit

---
+++ src/main/java/is/hi/darts/DartsApplication.java
@@ -0,0 +1,13 @@
+package is.hi.darts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/darts/DartsApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.darts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
