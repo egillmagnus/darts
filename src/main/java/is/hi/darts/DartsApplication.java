89caf980afd243cbf790c357c1cec041a817718e
Solvi Santos <solvisantos22@gmail.com>
2024-09-08 21:26:56 +0000

    bang

--- src/main/java/is/hi/darts/DartsApplication.java
+++ src/main/java/is/hi/darts/DartsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DartsApplication {
 
     //"Bulldarts" - Ari
+    //this is team member Sölvi Santos
     public static void main(String[] args) {
         SpringApplication.run(DartsApplication.class, args);
     }
