ebaca524bc1ed6e0b92eac2baa5c5433416b7669
Ari Gunnar Kristjónsson <agk19@hi.is>
2024-09-20 12:56:29 +0000

    fuckin with the dependancy

--- src/main/java/is/hi/darts/service/UserService.java
+++ src/main/java/is/hi/darts/service/UserService.java
@@ -1,4 +1,35 @@
 package is.hi.darts.service;
 
+
+import is.hi.darts.model.User;
+
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+    public User registerUser(String email, String username, String password) {
+        if (userRepository.existsByEmail(email)) {
+            throw new RuntimeException("Email is already registered.");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password)); // Hash the password
+        return userRepository.save(user);
+    }
+
+    public User loginUser(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid credentials.");
+        }
+        return user;
+    }
+
+
 }
