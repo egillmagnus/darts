88917b8f4aa23989b460f5d19ed99c5fbea3a6c3
Ari Gunnar Kristjónsson <agk19@hi.is>
2024-10-23 12:41:55 +0000

    Added dahsboard

--- src/main/java/is/hi/darts/controller/NavigationController.java
+++ src/main/java/is/hi/darts/controller/NavigationController.java
@@ -5,10 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class NavigationController {
 
     @GetMapping("/")
     public String home(Model model) {
         return "home";
     }
+
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {return "dashboard";}
 }
